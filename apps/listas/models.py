cbeef187e30733c597ba6cb296d1ad597e873d91
Giovana Bim Fernandes Moreira <giovana.fernandes@aluno.ifsertao-pe.edu.br>
2025-12-07 13:28:22 -0300

    Adicao do progresso no model listas

--- apps/listas/models.py
+++ apps/listas/models.py
@@ -14,5 +14,14 @@ class Lista(models.Model):
     # progresso = ?
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE) # mudei pare cascade pois nao irei manter o historio caso o user seja deletado
 
+    def calcular_progresso(self):
+        total_tarefas = self.tarefa_set.count() # Usa o nome 'tarefa_set' (relação inversa)
+        if total_tarefas == 0:
+            return 0
+        
+        tarefas_concluidas = self.tarefa_set.filter(concluido=True).count()
+        return (tarefas_concluidas / total_tarefas) * 100
+
     def __str__(self):
-        return f"{self.nome} - {self.importancia}" # tbm queria adicionar o progresso aq
+        progresso = round(self.calcular_progresso())
+        return f"{self.nome} - {self.importancia} - {progresso}%" # tbm queria adicionar o progresso aq
